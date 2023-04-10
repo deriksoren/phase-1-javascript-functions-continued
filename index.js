4cc8f9b8ba5632adacc3a992147a27468466603b
Erik Sorensen <deriksoren@gmail.com>
2023-04-10 11:33:38 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+function saturdayFun (n) {
+    if (n){
+        return `This Saturday, I want to ${n}!`;
+    } 
+    else {   
+    return `This Saturday, I want to roller-skate!`;
+    }
+}
+function mondayWork (n) {
+    if (n) {
+        return `This Monday, I will ${n}.`
+    }
+    else {
+        return `This Monday, I will go to the office.`
+    }
+}
+ function wrapAdjective (p) {
+    return function (n) {
+        if (n) {
+            return `You are ${p}${n}${p}!`
+        }
+        else {
+            return `You are ${p}special${p}!`
+        }
+    }
+ }   
